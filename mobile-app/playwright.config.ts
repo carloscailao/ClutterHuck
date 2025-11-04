0afbb4c8724953f33805ad6ff371a35ac7f637c9
Deo Diamante <diamantedeozamir@gmail.com>
2025-11-04 19:56:26 +0800

    update pw config/yml

--- mobile-app/playwright.config.ts
+++ mobile-app/playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-  timeout: 30 * 1000,
+  timeout: 60 * 1000,
   retries: 1,
   reporter: [['list'], ['html', { outputFolder: 'playwright-report', open: 'never' }]],
   use: {
