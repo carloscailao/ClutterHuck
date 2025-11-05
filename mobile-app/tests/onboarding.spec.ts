03178e1e53cf22f560ad123ae461a138289dbb92
julianna ♡ <juliannadelcarmen@gmail.com>
2025-11-06 06:06:41 +0800

    Update onboarding.spec.ts

--- mobile-app/tests/onboarding.spec.ts
+++ mobile-app/tests/onboarding.spec.ts
@@ -1,45 +1,32 @@
 import { test, expect } from '@playwright/test';
 
-test('skip to onboarding', async ({ page }) => {
-  await page.goto('/');
+test('onboarding', async ({ page }) => {
+    await page.goto('/');
 
-  // Skip to Onboarding
-  await page.getByText('Skip to Onboarding (Sample)').click();
+    // Click "Get Started" to move to the sign-up screen.
+    await page.getByText('Get Started').click();
+    
+    // Fill the email and password fields
+    await page.getByLabel('Email').fill('stsweng@gmail.com');
+    await page.getByLabel('Password').fill('SecurePassword123');
 
-  // Wait for UI to update
-  await page.waitForTimeout(10000);
+    // Click the main submission button
+    await page.getByText('Create Account').click();
 
-  // enter username
-  await page.click('input[placeholder*="username"]');  
-  await page.type('input[placeholder*="username"]', 'JohnDoe');
+    // Enter First Name, Last Name, Username
+    await page.getByLabel('First name').fill('ST');
+    await page.getByLabel('Last name').fill('SWENG');
+    await page.getByLabel('Username').fill('STSWENG123');
 
-  // Wait for UI to update
-  await page.waitForTimeout(10000);
+    // Wait UI to update
+    await page.waitForTimeout(1500);
 
-  // Click next
-  await page.getByText('Next').click();
+    // Click next
+    await page.getByText('Next').click();
 
-  // Click skip
-  // await page.locator('div.css-text-146c3p1', { hasText: /^Skip$/ }).click();
-  await page.getByText('Skip').first().click();
+    // Skip the profile picture upload
+    await page.getByText('Skip').click(); 
 
-  // enter organization name
-  await page.click('input[placeholder*="Search or type organization name"]');  
-  await page.type('input[placeholder*="Search or type organization name"]', 'TestOrg');
-
-  // Wait for UI to update
-  await page.waitForTimeout(10000);
-
-  // Click next
-  await page.getByText('Next').nth(1).click();
-
-  // Select privacy option
-  await page.getByText('Keep my profile private').click();
-
-  // Click finish
-  await page.getByText('Finish').click();
-
-  // Assert onboarding complete
-  await expect(page.getByText('Welcome to ClutterHuck', { exact: false })).toBeVisible({ timeout: 10000 });
-
+    // Assert that onboarding is complete by checking for a text element on the main app screen 
+    await expect(page.getByText('Welcome to ClutterHuck', { exact: false })).toBeVisible({ timeout: 10000 });
 });
